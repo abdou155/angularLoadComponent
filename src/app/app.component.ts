64a549a66ba79e269eafe6afe1fb9e6c3c5929af
fatnassi abdesslam <abdesslam155@gmail.com>
2021-02-20 22:24:34 +0100

    initalcommit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,55 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, QueryList, ViewChild, ViewChildren } from '@angular/core';
+import { FormGroup, FormControl, FormArray, FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'project';
+export class AppComponent  {
+  name = 'Angular';
+
+  productForm: FormGroup;
+
+  constructor(private fb:FormBuilder) {
+
+    this.productForm = this.fb.group({
+      name: '',
+      quantities: this.fb.array([]) ,
+    });
+  }
+
+  @ViewChildren('myinput') inputs:QueryList<ElementRef>
+
+
+  counter = 0 ;
+  quantities() : FormArray {
+    return this.productForm.get("quantities") as FormArray
+  }
+
+  newQuantity(): FormGroup {
+
+    return this.fb.group({
+      qty: ''
+    })
+
+  }
+
+  addQuantity() {
+    this.quantities().push(this.newQuantity());
+    this.inputs.last.nativeElement.focus()
+
+    this.inputs.changes.subscribe(() => {
+        this.inputs.last.nativeElement.focus()
+    })
+
+  }
+
+  removeQuantity(i:number) {
+    this.quantities().removeAt(i);
+  }
+
+  onSubmit() {
+    console.log(this.productForm.value);
+  }
 }
